a222a4e7e6c28186a706b6e60c2079077fc7ed01
[Sandeep] <sandeep977244@gmail.com>
2026-04-09 19:25:53 +0530

    Making a ui project using react props and Tailwind CSS

---
+++ 08_ui_project/src/components/Section1/Section1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Section1 = () => {
+  return (
+    <div> 
+         <div className='h-screen w-full bg-sky-800 text-black'>Section1</div>
+    </div>
+  )
+}
+
+export default Section1
---
+++ 08_ui_project/src/components/Section2/Section2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Section2 = () => {
+  return (
+    <div>
+      <div className='h-screen w-full bg-sky-400 text-black'>Section2</div>
+    </div>
+  )
+}
+
+export default Section2
