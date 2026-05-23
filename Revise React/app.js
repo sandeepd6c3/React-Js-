ba23997bf74aa43291e866384abc31e6ecc335e1
[Sandeep] <sandeep977244@gmail.com>
2026-05-23 17:46:36 +0530

    Add initial files for React project setup including app.js, index.html, and script.js

---
+++ Revise React/app.js
@@ -0,0 +1,9 @@
+ let user = "Sandeep"
+ 
+ export let skills = ["C++","DSA","REACTJS"]
+ 
+ export let age = 20;
+ 
+ export default user;
+ 
+ 
---
+++ Revise React/script.js
@@ -0,0 +1,12 @@
+import naam from './app.js'
+
+ console.log(naam);
+
+ import { skills } from './app.js';
+ console.log(skills);
+
+ import { age } from './app.js';
+console.log(age);
+
+
+ 
